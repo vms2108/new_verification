c33c4b1813507ff56a59ea9c04855cd9fcd79045
mikhail <vms2108@mail.ru>
2018-07-13 14:30:44 +0300

    new field component

--- src/app/form/field/field.component.ts
+++ src/app/form/field/field.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-field',
@@ -6,11 +6,24 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./field.component.scss']
 })
 export class FieldComponent implements OnInit {
-
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output() onChanged = new EventEmitter<string>();
   constructor() { }
   @Input() Value: string;
   @Input() Label: string;
+  @Input() Id: string;
+  on: string = null;
+  off: string = null;
+  neutral: string = null;
+  value: string = null;
   ngOnInit() {
+    this.on = 'on' + this.Id;
+    this.off = 'off' + this.Id;
+    this.neutral = 'neutral' + this.Id;
+    this.value = 'value' + this.Id;
+  }
+  change (value: string) {
+    this.onChanged.emit(value);
   }
 
 }
